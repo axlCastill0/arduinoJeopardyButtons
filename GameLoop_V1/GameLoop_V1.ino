0289f6c09a3f6417495a2130a4155f3f185a05ce
axl <axl.castilloblasco@gmail.com>
2024-10-10 21:25:29 -0400

    game loop from notes

--- GameLoop_V1/GameLoop_V1.ino
+++ GameLoop_V1/GameLoop_V1.ino
@@ -3,34 +3,99 @@ bool isAnswerState = false;
 unsigned int firstPin = -1;
 unsigned int answerTimeout = 7000;
 
-unsigned int player1Pin = 2;
-unsigned int player2Pin = 3;
-unsigned int player3Pin = 4;
-unsigned int player4Pin = 5;
-unsigned int player5Pin = 6;
-unsigned int player6Pin = 7;
+unsigned int player1Led = 2;
+unsigned int player2Led = 3;
+unsigned int player3Led = 4;
+unsigned int player4Led = 5;
+unsigned int player5Led = 6;
+unsigned int player6Led = 7;
+
+unsigned int player1Btn = A0;
+unsigned int player2Btn = A1;
+unsigned int player3Btn = A2;
+unsigned int player4Btn = A3;
+unsigned int player5Btn = A4;
+unsigned int player6Btn = A5;
+
+unsigned int hostPin = 8;
+unsigned int buzzerPin = 12;
 
 void setup() {
   Serial.begin(9600);
-  // put your setup code here, to run once:
-  pinMode(player1Pin, OUTPUT);
-  pinMode(player2Pin, OUTPUT);
-  pinMode(player3Pin, OUTPUT);
-  pinMode(player4Pin, OUTPUT);
-  pinMode(player5Pin, OUTPUT);
-  pinMode(player6Pin, OUTPUT);
+
+  pinMode(player1Led, OUTPUT);
+  pinMode(player2Led, OUTPUT);
+  pinMode(player3Led, OUTPUT);
+  pinMode(player4Led, OUTPUT);
+  pinMode(player5Led, OUTPUT);
+  pinMode(player6Led, OUTPUT);
+  pinMode(hostPin, INPUT_PULLUP);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(String(digitalRead(A0)));
-  if(digitalRead(A0) == 0) {
-    digitalWrite(2, HIGH);
+  if(isQuestionState) {
+    if(!isAnswerState) {
+      turnOffPlayerButtons();
+    }
   } else {
-    digitalWrite(2, LOW);
+    turnOnPlayerButtons();
+  }
+
+  if(digitalRead(hostPin) == 0 && !isQuestionState) {
+    isQuestionState = true;
+  } else {
+    digitalWrite(buzzerPin, HIGH);
+    isAnswerState = true;
+    turnOnPlayerButtons();
+  }
+
+  if(isAnswerState) {
+    if(firstPin == -1) {
+      readPins()
+    } else {
+      Serial.print(String(firstPin));
+      delay(2000);
+    }
   }
 }
 
 void turnOnPlayerButtons() {
+  digitalWrite(player1Led, HIGH);
+  digitalWrite(player2Led, HIGH);
+  digitalWrite(player3Led, HIGH);
+  digitalWrite(player4Led, HIGH);
+  digitalWrite(player5Led, HIGH);
+  digitalWrite(player6Led, HIGH);
+}
 
+void turnOffPlayerButtons() {
+  digitalWrite(player1Led, LOW);
+  digitalWrite(player2Led, LOW);
+  digitalWrite(player3Led, LOW);
+  digitalWrite(player4Led, LOW);
+  digitalWrite(player5Led, LOW);
+  digitalWrite(player6Led, LOW);
+}
+
+void readPins() {
+  if (firstPin == -1) {
+    if (digitalRead(player1Btn) == HIGH) {
+      firstPin = player1Led;
+    } else if (digitalRead(player2Btn) == HIGH) {
+      firstPin = player2Led;
+    } else if (digitalRead(player3Btn) == HIGH) {
+      firstPin = player3Led;
+    } else if (digitalRead(player4Btn) == HIGH) {
+      firstPin = player4Led;
+    } else if (digitalRead(player5Btn) == HIGH) {
+      firstPin = player5Led;
+    } else if (digitalRead(player6Btn) == HIGH) {
+      firstPin = player6Led;
+    }
+    
+    if (firstPin != -1) {
+      Serial.print("First signal received from pin: ");
+      Serial.println(firstPin);
+    }
 }
