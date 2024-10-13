0833d254c4a46fcf5052129443315d62341179bc
axl <axl.castilloblasco@gmail.com>
2024-10-13 08:31:01 -0400

    wip

--- GameLoop_V1/GameLoop_V1.ino
+++ GameLoop_V1/GameLoop_V1.ino
@@ -2,13 +2,14 @@ bool isQuestionState = false;
 bool isAnswerState = false;
 unsigned int firstPin = -1;
 unsigned int answerTimeout = 7000;
+unsigned int time;
 
-unsigned int player1Led = 2;
-unsigned int player2Led = 3;
-unsigned int player3Led = 4;
-unsigned int player4Led = 5;
-unsigned int player5Led = 6;
-unsigned int player6Led = 7;
+unsigned int player1Pwr = 2;
+unsigned int player2Pwr = 3;
+unsigned int player3Pwr = 4;
+unsigned int player4Pwr = 5;
+unsigned int player5Pwr = 6;
+unsigned int player6Pwr = 7;
 
 unsigned int player1Btn = A0;
 unsigned int player2Btn = A1;
@@ -23,12 +24,22 @@ unsigned int buzzerPin = 12;
 void setup() {
   Serial.begin(9600);
 
-  pinMode(player1Led, OUTPUT);
-  pinMode(player2Led, OUTPUT);
-  pinMode(player3Led, OUTPUT);
-  pinMode(player4Led, OUTPUT);
-  pinMode(player5Led, OUTPUT);
-  pinMode(player6Led, OUTPUT);
+  time = millis();
+
+  pinMode(player1Pwr, OUTPUT);
+  pinMode(player2Pwr, OUTPUT);
+  pinMode(player3Pwr, OUTPUT);
+  pinMode(player4Pwr, OUTPUT);
+  pinMode(player5Pwr, OUTPUT);
+  pinMode(player6Pwr, OUTPUT);
+
+  pinMode(player1Btn, INPUT_PULLUP);
+  pinMode(player2Btn, INPUT_PULLUP);
+  pinMode(player3Btn, INPUT_PULLUP);
+  pinMode(player4Btn, INPUT_PULLUP);
+  pinMode(player5Btn, INPUT_PULLUP);
+  pinMode(player6Btn, INPUT_PULLUP);
+
   pinMode(hostPin, INPUT_PULLUP);
   pinMode(buzzerPin, OUTPUT);
 }
@@ -64,37 +75,37 @@ void loop() {
 }
 
 void turnOnPlayerButtons() {
-  digitalWrite(player1Led, HIGH);
-  digitalWrite(player2Led, HIGH);
-  digitalWrite(player3Led, HIGH);
-  digitalWrite(player4Led, HIGH);
-  digitalWrite(player5Led, HIGH);
-  digitalWrite(player6Led, HIGH);
+  digitalWrite(player1Pwr, HIGH);
+  digitalWrite(player2Pwr, HIGH);
+  digitalWrite(player3Pwr, HIGH);
+  digitalWrite(player4Pwr, HIGH);
+  digitalWrite(player5Pwr, HIGH);
+  digitalWrite(player6Pwr, HIGH);
 }
 
 void turnOffPlayerButtons() {
-  digitalWrite(player1Led, LOW);
-  digitalWrite(player2Led, LOW);
-  digitalWrite(player3Led, LOW);
-  digitalWrite(player4Led, LOW);
-  digitalWrite(player5Led, LOW);
-  digitalWrite(player6Led, LOW);
+  digitalWrite(player1Pwr, LOW);
+  digitalWrite(player2Pwr, LOW);
+  digitalWrite(player3Pwr, LOW);
+  digitalWrite(player4Pwr, LOW);
+  digitalWrite(player5Pwr, LOW);
+  digitalWrite(player6Pwr, LOW);
 }
 
 void readPins() {
   if (firstPin == -1) {
     if (digitalRead(player1Btn) == HIGH) {
-      firstPin = player1Led;
+      firstPin = player1Pwr;
     } else if (digitalRead(player2Btn) == HIGH) {
-      firstPin = player2Led;
+      firstPin = player2Pwr;
     } else if (digitalRead(player3Btn) == HIGH) {
-      firstPin = player3Led;
+      firstPin = player3Pwr;
     } else if (digitalRead(player4Btn) == HIGH) {
-      firstPin = player4Led;
+      firstPin = player4Pwr;
     } else if (digitalRead(player5Btn) == HIGH) {
-      firstPin = player5Led;
+      firstPin = player5Pwr;
     } else if (digitalRead(player6Btn) == HIGH) {
-      firstPin = player6Led;
+      firstPin = player6Pwr;
     }
     
     if (firstPin != -1) {
